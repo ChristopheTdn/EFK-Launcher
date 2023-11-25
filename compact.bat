afe35e4ccfad5dad530ae7907d8f479e058077b1
ToF <Christophe.taudin@gmail.com>
2023-11-25 19:00:24 +0100

    gestionnaire de deploiement

---
+++ compact.bat
@@ -0,0 +1,5 @@
+pyinstaller "EFK.spec" --distpath "EFKLauncher"
+Robocopy.exe "./config" "./EFKLauncher/config" /E /MIR /Z /DCOPY:T  /TIMFIX /R:0 /W:0 /XF "config.json"
+cd "EFKLauncher"
+WinRAR a EFKLauncher.zip  "EFK Launcher.exe" "./config"
+
