96faa73c46f77730ae277543f89e0f504f5f2301
ToF <Christophe.taudin@gmail.com>
2023-12-17 12:05:06 +0100

    Traduction Espagnole par ElPacsWTF

--- translations/es-ES.ts
+++ translations/es-ES.ts
@@ -1,22 +1,22 @@
 <?xml version="1.0" encoding="utf-8"?>
 <!DOCTYPE TS>
-<TS version="2.1" language="es_ES" sourcelanguage="_FR">
+<TS version="2.1" language="es_ES" sourcelanguage="fr_FR">
 <context>
     <name>Fenetre_Principale</name>
     <message>
         <location filename="../principale.ui" line="66"/>
         <source>Installation</source>
-        <translation>Installation</translation>
+        <translation>Instalación</translation>
     </message>
     <message>
         <location filename="../principale.ui" line="78"/>
         <source>MOD Manager</source>
-        <translation>MOD Manager</translation>
+        <translation>Gestor de MODS</translation>
     </message>
     <message>
         <location filename="../principale.ui" line="123"/>
         <source>Préreglage Difficulté</source>
-        <translation>Difficult settings</translation>
+        <translation>Configuraciones de dificultad</translation>
     </message>
     <message>
         <location filename="../principale.ui" line="135"/>
@@ -41,7 +41,7 @@
     <message>
         <location filename="../principale.ui" line="431"/>
         <source>&lt;html&gt;&lt;head/&gt;&lt;body&gt;&lt;p&gt;&lt;span style=&quot; font-weight:700; text-decoration: underline;&quot;&gt;Pour desintaller EFK :&lt;br/&gt;&lt;/span&gt;1 - Quitter Project Zomboid&lt;br/&gt;2 - Se Désabonner à la &lt;a href=&quot;https://steamcommunity.com/workshop/filedetails/?id=3048855836&quot;&gt;&lt;span style=&quot; text-decoration: underline; color:#0000ff;&quot;&gt;collection Steam EFK&lt;br/&gt;&lt;/span&gt;&lt;/a&gt;3 - Cliquer sur &lt;span style=&quot; font-weight:700;&quot;&gt;Desinstaller EFK&lt;br/&gt;&lt;/span&gt;4 - Une fois le EFK Launcher Fermé&lt;br/&gt;Supprimer votre repertoire local du &lt;span style=&quot; font-weight:700;&quot;&gt;EFK Launcher&lt;/span&gt;&lt;/p&gt;&lt;p&gt;&lt;span style=&quot; font-weight:700;&quot;&gt;&lt;br/&gt;&lt;/span&gt;&lt;/p&gt;&lt;/body&gt;&lt;/html&gt;</source>
-        <translation>&lt;html&gt;&lt;head/&gt;&lt;body&gt;&lt;p&gt;&lt;span style=&quot; font-weight:700; text-decoration: underline;&quot;&gt;Uninstall EFK :&lt;br/&gt;&lt;/span&gt;1 - Quit Project Zomboid&lt;br/&gt;2 - Unsubscribe &lt;span style=&quot; text-decoration: underline; color:#0000ff;&quot;&gt;collection Steam EFK&lt;br/&gt;&lt;/span&gt;3 - Click &lt;span style=&quot; font-weight:700;&quot;&gt;Uninstall EFK button&lt;br/&gt;&lt;/span&gt;4 - when  EFK Launcher closed&lt;br/&gt;delete your &lt;span style=&quot; font-weight:700; text-decoration: underline;&quot;&gt;EFK Launcher directory&lt;/span&gt;&lt;/p&gt;&lt;/body&gt;&lt;/html&gt;</translation>
+        <translation>&lt;html&gt;&lt;head/&gt;&lt;body&gt;&lt;p&gt;&lt;span style=&quot; font-weight:700; text-decoration: underline;&quot;&gt;Desinstalar  EFK :&lt;br/&gt;&lt;/span&gt;1 - Salir de Project Zomboid&lt;br/&gt;2 - Desuscribirse &lt;span style=&quot; text-decoration: underline; color:#0000ff;&quot;&gt;EFK Colección en Steam &lt;br/&gt;&lt;/span&gt;3 - Click &lt;span style=&quot; font-weight:700;&quot;&gt;en el boton Densistalar EFK&lt;br/&gt;&lt;/span&gt;4 - Cuando el EFK Launcher se cierre&lt;br/&gt;borra tu &lt;span style=&quot; font-weight:700; text-decoration: underline;&quot;&gt;Tu directorio de EFK Launcher&lt;/span&gt;&lt;/p&gt;&lt;/body&gt;&lt;/html&gt;</translation>
     </message>
     <message>
         <location filename="../principale.ui" line="903"/>
@@ -61,32 +61,32 @@
     <message>
         <location filename="../principale.ui" line="1144"/>
         <source>Abonnements à la collection STEAM ESCAPE FROM KNOX COUNTY</source>
-        <translation>ESCAPE FROM KNOX COUNTY Steam Collection</translation>
+        <translation>Descargar la colección de mods de ESCAPE FROM KNOX COUNTY en Steam</translation>
     </message>
     <message>
         <location filename="../principale.ui" line="1224"/>
         <source>EFK Standard (Low CPU Power)</source>
-        <translation>EFK Standard (Low CPU Power)</translation>
+        <translation>EFK Standard (Para CPU de baja potencia)</translation>
     </message>
     <message>
         <location filename="../principale.ui" line="1237"/>
         <source>EFK Enhanced (High CPU Power)</source>
-        <translation>EFK Advanced (High CPU Power)</translation>
+        <translation>EFK Advanced (Para CPU de alta potencia)</translation>
     </message>
     <message>
         <location filename="../principale.ui" line="1275"/>
         <source>No Modif (For Modders Only)</source>
-        <translation>No Modif (For Modders Only)</translation>
+        <translation>No Modif (Solo para Modders)</translation>
     </message>
     <message>
         <location filename="../principale.ui" line="1295"/>
         <source>&lt;html&gt;&lt;head/&gt;&lt;body&gt;&lt;p&gt;&lt;span style=&quot; font-size:10pt; font-weight:700; text-decoration: underline; color:#aa0000;&quot;&gt;IMPORTANT :&lt;/span&gt;&lt;span style=&quot; font-weight:700; text-decoration: underline;&quot;&gt;&lt;br/&gt;&lt;/span&gt;If you&apos;re not a Modder,&lt;br/&gt;Please make a choice EFK Enhanced&lt;br/&gt;or EFK Standard &lt;span style=&quot; font-weight:700; text-decoration: underline;&quot;&gt;Before&lt;/span&gt; Launch PZ&lt;/p&gt;&lt;/body&gt;&lt;/html&gt;</source>
-        <translation>&lt;html&gt;&lt;head/&gt;&lt;body&gt;&lt;p&gt;&lt;span style=&quot; font-size:10pt; font-weight:700; text-decoration: underline; color:#aa0000;&quot;&gt;IMPORTANT :&lt;/span&gt;&lt;span style=&quot; font-weight:700; text-decoration: underline;&quot;&gt;&lt;br/&gt;&lt;/span&gt;If &lt;span style=&quot; font-weight:700; text-decoration: underline;&quot;&gt;you&apos;re not a Modder&lt;/span&gt;,&lt;br/&gt;Please make a choice EFK Enhanced&lt;br/&gt;or EFK Standard &lt;span style=&quot; font-weight:700; text-decoration: underline;&quot;&gt;Before&lt;/span&gt; Launch PZ&lt;/p&gt;&lt;/body&gt;&lt;/html&gt;</translation>
+        <translation>&lt;html&gt;&lt;head/&gt;&lt;body&gt;&lt;p&gt;&lt;span style=&quot; font-size:10pt; font-weight:700; text-decoration: underline; color:#aa0000;&quot;&gt;IMPORTANTE :&lt;/span&gt;&lt;span style=&quot; font-weight:700; text-decoration: underline;&quot;&gt;&lt;br/&gt;&lt;/span&gt;If &lt;span style=&quot; font-weight:700; text-decoration: underline;&quot;&gt;you&apos;re not a Modder&lt;/span&gt;,&lt;br/&gt;Please make a choice EFK Enhanced&lt;br/&gt;or EFK Standard &lt;span style=&quot; font-weight:700; text-decoration: underline;&quot;&gt;Before&lt;/span&gt; Launch PZ&lt;/p&gt;&lt;/body&gt;&lt;/html&gt;</translation>
     </message>
     <message>
         <location filename="../principale.ui" line="729"/>
         <source>Repertoires Utilisateur</source>
-        <translation>User Directories</translation>
+        <translation>Directorio del usuario</translation>
     </message>
     <message>
         <location filename="../principale.ui" line="37"/>
@@ -96,7 +96,7 @@
     <message>
         <location filename="../principale.ui" line="109"/>
         <source>&lt;html&gt;&lt;head/&gt;&lt;body&gt;&lt;p align=&quot;center&quot;&gt;Ajout Profil &lt;span style=&quot; font-weight:700;&quot;&gt;STANDARD&lt;/span&gt; et &lt;span style=&quot; font-weight:700;&quot;&gt;ADVANCED&lt;/span&gt; à &lt;span style=&quot; font-weight:700; font-style:italic;&quot;&gt;Lua/saved_modlists.txt&lt;/span&gt;&lt;/p&gt;&lt;/body&gt;&lt;/html&gt;</source>
-        <translation>&lt;html&gt;&lt;head/&gt;&lt;body&gt;&lt;p align=&quot;center&quot;&gt;Add Profile &lt;span style=&quot; font-weight:700;&quot;&gt;STANDARD&lt;/span&gt; et &lt;span style=&quot; font-weight:700;&quot;&gt;ADVANCED&lt;/span&gt; to &lt;span style=&quot; font-weight:700; font-style:italic;&quot;&gt;Lua/saved_modlists.txt&lt;/span&gt;&lt;/p&gt;&lt;/body&gt;&lt;/html&gt;</translation>
+        <translation>&lt;html&gt;&lt;head/&gt;&lt;body&gt;&lt;p align=&quot;center&quot;&gt;Agregar el perfil &lt;span style=&quot; font-weight:700;&quot;&gt;STANDARD&lt;/span&gt; y &lt;span style=&quot; font-weight:700;&quot;&gt;ADVANCED&lt;/span&gt; en &lt;span style=&quot; font-weight:700; font-style:italic;&quot;&gt;Lua/saved_modlists.txt&lt;/span&gt;&lt;/p&gt;&lt;/body&gt;&lt;/html&gt;</translation>
     </message>
     <message>
         <location filename="../principale.ui" line="247"/>
@@ -116,48 +116,48 @@
     <message>
         <location filename="../principale.ui" line="279"/>
         <source>Options</source>
-        <translation>Options</translation>
+        <translation>Opciones</translation>
     </message>
     <message>
         <location filename="../principale.ui" line="291"/>
         <source>Mise a Jour EFK Launcher</source>
-        <translation>Update EFK Launcher</translation>
+        <translation>Actualizar EFK Launcher</translation>
     </message>
     <message>
         <location filename="../principale.ui" line="311"/>
         <source>Maj EFK Launcher</source>
-        <translation>Update EFK Launcher</translation>
+        <translation>Actualizar EFK Launcher</translation>
     </message>
     <message>
         <location filename="../principale.ui" line="334"/>
         <source>&lt;html&gt;&lt;head/&gt;&lt;body&gt;&lt;p&gt;&lt;span style=&quot; font-weight:700; text-decoration: underline;&quot;&gt;MaJ EFK :&lt;/span&gt;&lt;/p&gt;&lt;p&gt;Pour mettre a jour &lt;span style=&quot; font-weight:700;&quot;&gt;EFK Launcher&lt;/span&gt;,&lt;br/&gt;et les scripts de config EFK.&lt;br/&gt;&lt;/p&gt;&lt;p&gt;&lt;br/&gt;&lt;/p&gt;&lt;/body&gt;&lt;/html&gt;</source>
-        <translation>&lt;html&gt;&lt;head/&gt;&lt;body&gt;&lt;p&gt;&lt;span style=&quot; font-weight:700; text-decoration: underline;&quot;&gt;Update EFK :&lt;/span&gt;&lt;/p&gt;&lt;p&gt;To update &lt;span style=&quot; font-weight:700;&quot;&gt;EFK Launcher&lt;/span&gt;,&lt;br/&gt;and EFK config Scripts.&lt;br/&gt;&lt;/p&gt;&lt;p&gt;&lt;br/&gt;&lt;/p&gt;&lt;/body&gt;&lt;/html&gt;</translation>
+        <translation>&lt;html&gt;&lt;head/&gt;&lt;body&gt;&lt;p&gt;&lt;span style=&quot; font-weight:700; text-decoration: underline;&quot;&gt;Actualizar EFK :&lt;/span&gt;&lt;/p&gt;&lt;p&gt;Actualizar &lt;span style=&quot; font-weight:700;&quot;&gt;EFK Launcher&lt;/span&gt;,&lt;br/&gt;y configura EFK Scripts.&lt;br/&gt;&lt;/p&gt;&lt;p&gt;&lt;br/&gt;&lt;/p&gt;&lt;/body&gt;&lt;/html&gt;</translation>
     </message>
     <message>
         <location filename="../principale.ui" line="347"/>
         <source>&lt;html&gt;&lt;head/&gt;&lt;body&gt;&lt;p&gt;&lt;img src=&quot;:/gfx/gfx/supprimer.png&quot; width=&quot;22&quot;/&gt;&lt;span style=&quot; font-size:12pt; vertical-align:super;&quot;&gt;Serveur MAJ non trouvé&lt;/span&gt;&lt;/p&gt;&lt;/body&gt;&lt;/html&gt;</source>
-        <translation>&lt;html&gt;&lt;head/&gt;&lt;body&gt;&lt;p&gt;&lt;img src=&quot;:/gfx/gfx/supprimer.png&quot; width=&quot;22&quot;/&gt;&lt;span style=&quot; font-size:12pt; vertical-align:super;&quot;&gt;Update Server not found&lt;/span&gt;&lt;/p&gt;&lt;/body&gt;&lt;/html&gt;</translation>
+        <translation>&lt;html&gt;&lt;head/&gt;&lt;body&gt;&lt;p&gt;&lt;img src=&quot;:/gfx/gfx/supprimer.png&quot; width=&quot;22&quot;/&gt;&lt;span style=&quot; font-size:12pt; vertical-align:super;&quot;&gt;Servidor de actualización no encontrado&lt;/span&gt;&lt;/p&gt;&lt;/body&gt;&lt;/html&gt;</translation>
     </message>
     <message>
         <location filename="../principale.ui" line="363"/>
         <location filename="../principale.ui" line="1328"/>
         <source>&lt;html&gt;&lt;head/&gt;&lt;body&gt;&lt;p&gt;&lt;img src=&quot;:/gfx/gfx/danger.png&quot; width=&quot;20&quot;/&gt;&lt;span style=&quot; font-size:14pt; font-weight:700; color:#ff5500; vertical-align:super;&quot;&gt;Mise a jour disponible&lt;/span&gt;&lt;/p&gt;&lt;/body&gt;&lt;/html&gt;</source>
-        <translation>&lt;html&gt;&lt;head/&gt;&lt;body&gt;&lt;p&gt;&lt;img src=&quot;:/gfx/gfx/danger.png&quot; width=&quot;20&quot;/&gt;&lt;span style=&quot; font-size:14pt; font-weight:700; color:#ff5500; vertical-align:super;&quot;&gt;Update available&lt;/span&gt;&lt;/p&gt;&lt;/body&gt;&lt;/html&gt;</translation>
+        <translation>&lt;html&gt;&lt;head/&gt;&lt;body&gt;&lt;p&gt;&lt;img src=&quot;:/gfx/gfx/danger.png&quot; width=&quot;20&quot;/&gt;&lt;span style=&quot; font-size:14pt; font-weight:700; color:#ff5500; vertical-align:super;&quot;&gt;Actualización disponible&lt;/span&gt;&lt;/p&gt;&lt;/body&gt;&lt;/html&gt;</translation>
     </message>
     <message>
         <location filename="../principale.ui" line="383"/>
         <source>Désinstaller EFK</source>
-        <translation>Uninstall EFK</translation>
+        <translation>Desinstalar EFK</translation>
     </message>
     <message>
         <location filename="../principale.ui" line="403"/>
         <source>Desinstaller EFK</source>
-        <translation>Uninstall EFK</translation>
+        <translation>Desinstalar EFK</translation>
     </message>
     <message>
         <location filename="../principale.ui" line="438"/>
         <source>A Propos</source>
-        <translation>About</translation>
+        <translation>Acerca de</translation>
     </message>
     <message>
         <location filename="../principale.ui" line="482"/>
@@ -167,22 +167,22 @@
     <message>
         <location filename="../principale.ui" line="495"/>
         <source>Code :</source>
-        <translation>Coder :</translation>
+        <translation>Programador :</translation>
     </message>
     <message>
         <location filename="../principale.ui" line="508"/>
         <source>Traduction :</source>
-        <translation></translation>
+        <translation>Traduccion :</translation>
     </message>
     <message>
         <location filename="../principale.ui" line="560"/>
         <source>Source Code :</source>
-        <translation></translation>
+        <translation>Código fuente :</translation>
     </message>
     <message>
         <location filename="../principale.ui" line="573"/>
         <source>Project : </source>
-        <translation></translation>
+        <translation>Disocrd EFK :</translation>
     </message>
     <message>
         <location filename="../principale.ui" line="1359"/>
@@ -194,47 +194,47 @@
     <message>
         <location filename="../principale.ui" line="1478"/>
         <source>Active Bouton Wipe Map</source>
-        <translation>Unlock Bouton Wipe Map</translation>
+        <translation>Desbloquear el Boton Reinicar Mapas</translation>
     </message>
     <message>
         <location filename="../principale.ui" line="1010"/>
         <source>&lt;html&gt;&lt;head/&gt;&lt;body&gt;&lt;p&gt;&lt;span style=&quot; font-size:10pt; font-weight:700; text-decoration: underline; color:#aa0000;&quot;&gt;IMPORTANT :&lt;/span&gt;&lt;span style=&quot; font-weight:700; text-decoration: underline;&quot;&gt;&lt;br/&gt;&lt;/span&gt;&lt;span style=&quot; font-weight:700;&quot;&gt;- &lt;/span&gt;Entre 2 raids&lt;br/&gt;- personnage dans la base&lt;br/&gt;- Quitter PZ ou au Menu principal&lt;/p&gt;&lt;/body&gt;&lt;/html&gt;</source>
-        <translation>&lt;html&gt;&lt;head/&gt;&lt;body&gt;&lt;p&gt;&lt;span style=&quot; font-size:10pt; font-weight:700; text-decoration: underline; color:#aa0000;&quot;&gt;IMPORTANT :&lt;/span&gt;&lt;span style=&quot; font-weight:700; text-decoration: underline;&quot;&gt;&lt;br/&gt;&lt;/span&gt;&lt;span style=&quot; font-weight:700;&quot;&gt;- &lt;/span&gt;Between 2 raids&lt;br/&gt;- player in the base&lt;br/&gt;-Main Menu or Quit PZ&lt;/p&gt;&lt;/body&gt;&lt;/html&gt;</translation>
+        <translation>&lt;html&gt;&lt;head/&gt;&lt;body&gt;&lt;p&gt;&lt;span style=&quot; font-size:10pt; font-weight:700; text-decoration: underline; color:#aa0000;&quot;&gt;IMPORTANTE :&lt;/span&gt;&lt;span style=&quot; font-weight:700; text-decoration: underline;&quot;&gt;&lt;br/&gt;&lt;/span&gt;&lt;span style=&quot; font-weight:700;&quot;&gt;- &lt;/span&gt;Despues de 2 raids&lt;br/&gt;- El jugador en el hideout&lt;br/&gt;-Debe ir al menu principal o salir de PZ&lt;/p&gt;&lt;/body&gt;&lt;/html&gt;</translation>
     </message>
     <message>
         <location filename="../principale.ui" line="1029"/>
         <source>Chaine Twitch de Tancred terror</source>
-        <translation>Twitch channel TancredTerror</translation>
+        <translation>Twitch de Tancred terror</translation>
     </message>
     <message>
         <location filename="../principale.ui" line="1055"/>
         <source>Chaine YOUTUBE de Tancred terror</source>
-        <translation>YouTube channel TancredTerror</translation>
+        <translation>YOUTUBE de Tancred terror</translation>
     </message>
     <message>
         <location filename="../principale.ui" line="1087"/>
         <source>DISCORD de Tancred terror</source>
-        <translation>DISCORD server</translation>
+        <translation>DISCORD de EFK</translation>
     </message>
     <message>
         <location filename="../principale.ui" line="1176"/>
         <source>RUN PZ</source>
-        <translation>Launch PZ</translation>
+        <translation>Jugar PZ</translation>
     </message>
     <message>
         <location filename="../principale.ui" line="1199"/>
         <source>Mode DEBUG</source>
-        <translation>Debug Mode</translation>
+        <translation>Modo Debug</translation>
     </message>
     <message>
         <location filename="../principale.ui" line="1212"/>
         <source>EFK Performances impact</source>
-        <translation>EFK Performances impact</translation>
+        <translation>EFK Performance del juego</translation>
     </message>
     <message>
         <location filename="../principale.ui" line="820"/>
         <source>Profil PZ :</source>
-        <translation>PZ Profil :</translation>
+        <translation>Perfil PZ :</translation>
     </message>
     <message>
         <location filename="../principale.ui" line="836"/>
@@ -245,12 +245,12 @@
         <location filename="../principale.ui" line="852"/>
         <location filename="../principale.ui" line="919"/>
         <source>choisir</source>
-        <translation>Find</translation>
+        <translation>Buscar</translation>
     </message>
     <message>
         <location filename="../principale.ui" line="990"/>
         <source>WIPE MAP</source>
-        <translation>WIPE MAP</translation>
+        <translation>Reiniciar Mapas</translation>
     </message>
 </context>
 </TS>
