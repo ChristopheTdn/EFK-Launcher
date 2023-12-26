4b274e4691a46e116558a9d9717ec6bec0c0e502
ToF <Christophe.taudin@gmail.com>
2023-12-26 18:09:18 +0100

    Compilation Nuitka

--- compact.bat
+++ compact.bat
@@ -1,5 +1,6 @@
-pyinstaller EFK.py --distpath "EFKLauncher" 
+python -m nuitka EFK.py --onefile  --windows-icon-from-ico="EFK Launcher.ico" --enable-plugin=pyside6 --output-filename="EFK Launcher.exe" --include-module=EFK --disable-console
 Robocopy.exe "./config" "./EFKLauncher/config" /E /MIR /Z /DCOPY:T  /TIMFIX /R:0 /W:0 /XF "config.json"
+Robocopy.exe "./" "./EFKLauncher/" /MOV "EFK Launcher.exe"
 cd "EFKLauncher"
 WinRAR a EFKLauncher.zip  "EFK Launcher.exe" "./config"
 
