ead1f5eb23b405d76c217b311d40e102c9d528e1
ToF <Christophe.taudin@gmail.com>
2023-12-28 21:15:20 +0100

    1.67

--- compact.bat
+++ compact.bat
@@ -1,4 +1,4 @@
-python -m nuitka EFK.py --onefile  --windows-icon-from-ico="EFK Launcher.ico" --enable-plugin=pyside6 --output-filename="EFK Launcher.exe" --include-module=EFK --disable-console
+pyinstaller "EFK.spec" --distpath "EFKLauncher" 
 Robocopy.exe "./config" "./EFKLauncher/config" /E /MIR /Z /DCOPY:T  /TIMFIX /R:0 /W:0 /XF "config.json"
 Robocopy.exe "./" "./EFKLauncher/" /MOV "EFK Launcher.exe"
 cd "EFKLauncher"
