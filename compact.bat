8cc05c3c95ae209c77fd3922f6eb79b70dafa086
ToF <Christophe.taudin@gmail.com>
2024-01-24 13:43:27 +0100

    v1.72

--- compact.bat
+++ compact.bat
@@ -2,5 +2,7 @@ pyinstaller "EFK.spec" --distpath "EFKLauncher"
 Robocopy.exe "./config" "./EFKLauncher/config" /E /MIR /Z /DCOPY:T  /TIMFIX /R:0 /W:0 /XF "config.json"
 Robocopy.exe "./" "./EFKLauncher/" /MOV "EFK Launcher.exe"
 cd "EFKLauncher"
-WinRAR a EFKLauncher.zip  "EFK Launcher.exe" "./config"
+Robocopy.exe "./config/EFKLauncher" "./" "version.txt"
+winrar a EFKLauncher.zip  "EFK Launcher.exe" "./config"
+
 
